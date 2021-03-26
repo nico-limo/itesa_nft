05f9ff8ab38fd67180ea9aed68871410d9b5153f
maxsznaider <maxsznaider@yahoo.com>
2021-03-26 17:31:28 -0300

    Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -9,11 +9,58 @@ const Home = () => {
     <>
       <div className={styles.homeTitle}>Home</div>
       <div className={styles.homeOnSaleOrSold}>
-        <button>On Sale</button>
-        <button>Sold</button>
+        <button
+          className={`${showOnSale ? styles.selected : ""}`}
+          onClick={() => setShowOnSale(true)}
+        >
+          On Sale
+        </button>
+        <button
+          className={`${showOnSale ? "" : styles.selected}`}
+          onClick={() => setShowOnSale(false)}
+        >
+          Sold
+        </button>
       </div>
-      <hr />
       <div className={styles.homeGalleryContainer}>
+        <div className={styles.homeSingleArtworkContainer}>
+          <img
+            className={styles.homeSingleArtworkImage}
+            src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+            alt=""
+          />
+          <div className={styles.homeSingleArtworkTitle}>The Cube</div>
+          <div className={styles.homeSingleArtworkCreator}>
+            <img
+              className={styles.homeSingleArtworkCreatorAvatar}
+              src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+            />
+            <div>@deeple</div>
+          </div>
+          <div className={styles.homeSingleArtworkPrice}>
+            {showOnSale ? "Price" : "Sold For"}
+            <div className={styles.homeSingleArtworkPriceAmount}>2.50 ETH</div>
+          </div>
+        </div>
+        <div className={styles.homeSingleArtworkContainer}>
+          <img
+            className={styles.homeSingleArtworkImage}
+            src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+            alt=""
+          />
+          <div className={styles.homeSingleArtworkTitle}>The Cube</div>
+          <div className={styles.homeSingleArtworkCreator}>
+            <img
+              className={styles.homeSingleArtworkCreatorAvatar}
+              src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+            />
+            <div>@deeple</div>
+          </div>
+          <div className={styles.homeSingleArtworkPrice}>
+            {showOnSale ? "Price" : "Sold For"}
+            <div className={styles.homeSingleArtworkPriceAmount}>2.50 ETH</div>
+          </div>
+        </div>
         <div className={styles.homeSingleArtworkContainer}>
           <img
             className={styles.homeSingleArtworkImage}
