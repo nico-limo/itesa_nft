8cc96e9cb2cab7189a9d304ed8c60e88cf3e87f5
toiaa <victoria@geddes.com.ar>
2021-03-26 15:36:49 -0300

    register view

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,7 +1,42 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+import form from "../styles/Form.module.css";
 
-const Login = () => (
-    <div>Login</div>
-)
+const Register = () => (
+  <div className={form.container}>
+    <h2 className={form.title}>REGISTER</h2>
+    <div>
+      <form className={form.form}>
+        <input
+          className={form.input}
+          name="username"
+          placeholder="Enter a username"
+          type="text"
+        />
+        <input
+          className={form.input}
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+        />
+        <input
+          className={form.input}
+          name="password"
+          placeholder="Set a new password"
+          type="password"
+        />
+        <button>Create an Account</button>
+      </form>
+      <div>
+        <span>
+          <Link className={form.link} to="/login">
+            Login
+          </Link>
+        </span>
+        <span>Forgot password?</span>
+      </div>
+    </div>
+  </div>
+);
 
-export default Login
+export default Register;
