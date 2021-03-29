642ecf00a2af2f023b8402e0e695bbcb371a5854
nico-limo <nicolas.limonoff@gmail.com>
2021-03-29 14:25:50 -0300

    deprecated add

--- src/utils/firebase/requestFireStore.js
+++ src/utils/firebase/requestFireStore.js
@@ -2,12 +2,29 @@ import { db } from "../../firebaseConfig";
 
 // Collections
 //const artWorkRef = db.collection('artWork');
-const usersReference = db.collection('Users');
+const usersReference = db.collection('Users').doc()
 
+
+//---------------- USER FUNCTIONS------------------------
 export const newUser = async (user, username) => {
-  await usersReference.add({
+  await usersReference.set({
         email: user.user.email,
         username: username,
-        uid: user.user.uid
+        uid: user.user.uid,
+        created: new Date()
     })
 }
+
+export const updateUser = async () => {
+  const res = await usersReference.update
+}
+
+
+//---------------- USER FUNCTIONS------------------------
+
+//---------------- PRODUCT FUNCTIONS------------------------
+
+
+
+
+//---------------- PRODUCT FUNCTIONS------------------------
