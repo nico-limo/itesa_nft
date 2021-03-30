1a62c3a66c17a086f2e94e89788ab941196888af
maxsznaider <maxsznaider@yahoo.com>
2021-03-30 15:58:07 -0300

    SingleArtWork in Progress

--- src/containers/Artwork.jsx
+++ src/containers/Artwork.jsx
@@ -1,66 +1,39 @@
 import React, { useState, useEffect } from "react"
-import { useRecoilState } from "recoil";
+import { useRecoilState } from "recoil"
 
-import { userAtom } from "../state/atoms";
-import { ArtWorkFunctions } from "../utils/firebase/artWork";
+import { userAtom } from "../state/atoms"
+import { ArtWorkFunctions } from "../utils/firebase/artWork"
 import styles from "../styles/artWork.module.css"
 
 const Artwork = ({ match }) => {
-    const [user, setUser] = useRecoilState(userAtom)
-    const { artWork, getArtWork } = ArtWorkFunctions()
+  const [user, setUser] = useRecoilState(userAtom)
+  const { artWork, getArtWork } = ArtWorkFunctions()
 
-    // useEffect(() => {
-    //     getArtWork()
-    // }, [])
-    // // console.log("id", match.url)
-    // console.log(artWork)
-    // ._delegate._document.objectValue.proto.mapValue.fields
-
-    return (
-        <>
-            <div className={styles.singleArtworkContainer}>
-                <img
-                    className={styles.singleArtworkImage}
-                    src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
-                    alt=""
-                />
-            </div>
-            <div className={styles.divButtons}>
-                <button>
-                    @Deeple
-                </button>
-            </div>
-            <div className={styles.ArtFeaturesContainer}>
-                <div className={styles.artDescriptionsLeft}>
-                    <div className={styles.artworkTitle}>
-                        The Cube
-                    </div>
-                    <div className={styles.artworkSubtitle}>
-                        Description:
-                    </div>
-                    <div className={styles.artworkDescription}>
-                        "Suspended Past" // 2020
-                        Official artwork for Lamborghini.
-                        3860x5000 pixel
-                    </div>
-                </div>
-                <div className={styles.artDescriptionsRight}>
-                    
-                    <div className={styles.priceSubtitle}>
-                        Price:
-                    </div>  
-                    <div className={styles.artworkPrice}>
-                        5 ETH
-                    </div>  
-                </div>
-            </div>
-            <div className={styles.artDescriptionsLeft}>
-                <div className={styles.artworkTitle}>
-                    Creator
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.artworkTitle}>The Cube</div>
+      <div className={styles.singleArtworkContainer}>
+        <img
+          className={styles.singleArtworkImage}
+          src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+          alt=""
+        />
+      </div>
+      <div className={styles.ArtFeaturesContainer}>
+        <div className={styles.divButtons}>
+          <button>@deeple</button>
+        </div>
+        <div className={styles.artDescription}>
+          This little dude is part of my ongoing series of Friends. Why are they called Friends? The reason for that is easy. Every day the world shows its ugliest sides and sometimes it's just too much to handle. There are so many serious artworks out there on the cryptomarket and I want to give people something to smile about. </div>
+          <div className={styles.priceSubtitle}>Price:</div>
+          <div className={styles.artworkPrice}>5 ETH</div>
+          <button className={styles.buyButton}>Buy Now</button>
+      </div>
+      <div className={styles.artDescriptionsLeft}>
+        <div className={styles.artworkTitle}>Creator</div>
+      </div>
+    </>
+  )
 }
 
 export default Artwork
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,50 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 //styles
-import form from "../styles/Form.module.css";
+import form from "../styles/Form.module.css"
 //utils
-import { useInput } from "../utils/hooks/useInput";
-import { UserFunctions } from "../utils/firebase/authEmail";
+import { useInput } from "../utils/hooks/useInput"
+import { UserFunctions } from "../utils/firebase/authEmail"
 
 const Login = () => {
-  const email = useInput("email");
-  const password = useInput("password");
-  const { login } = UserFunctions();
+  const email = useInput("email")
+  const password = useInput("password")
+  const { login } = UserFunctions()
 
   return (
-    <div className={form.container}>
-      <h2 className={form.title}>SIGN IN</h2>
-      <div>
-        <form className={form.form}>
-          <input
-            className={form.input}
-            placeholder="Enter your email"
-            type="email"
-            {...email}
-          />
-          <input
-            className={form.input}
-            placeholder="Set a new password"
-            type="password"
-            {...password}
-          />
-          <button
-            onClick={(event) => login(event, email.value, password.value)}
-          >
-            Sign In
-          </button>
-        </form>
+    <>
+      <div className={form.title}>Log In</div>
+      <div className={form.container}>
         <div>
-          <span>
-            <Link className={form.link} to="/register">
-              Sign Up
-            </Link>
-          </span>
-          <span>Forgot password?</span>
+          <form className={form.form}>
+            <input
+              className={form.input}
+              placeholder="Enter your email"
+              type="email"
+              {...email}
+            />
+            <input
+              className={form.input}
+              placeholder="Set a new password"
+              type="password"
+              {...password}
+            />
+            <button
+              onClick={(event) => login(event, email.value, password.value)}
+            >
+              Sign In
+            </button>
+          </form>
+          <div>
+            <span>
+              <Link className={form.link} to="/register">
+                Sign Up
+              </Link>
+            </span>
+            <span className={form.forgotPassword}>Forgot password?</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default Login;
+export default Login
