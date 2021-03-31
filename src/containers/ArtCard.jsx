54887b3d047dfca4ee22cff595d48aa5c6cb3adc
maxsznaider <maxsznaider@yahoo.com>
2021-03-31 11:11:43 -0300

    Home HTML/CSS

--- src/containers/ArtCard.jsx
+++ src/containers/ArtCard.jsx
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css"
 const ArtCard = ({ piece }) => {
   return (
     <div key={piece.id} className={styles.homeSingleArtworkContainer}>
+      <div>
       <img
         className={styles.homeSingleArtworkImage}
         src={piece.imgURI}
@@ -13,10 +14,11 @@ const ArtCard = ({ piece }) => {
       <div className={styles.homeSingleArtworkCreator}>
         <img
           className={styles.homeSingleArtworkCreatorAvatar}
-          src="http://www.fubiz.net/wp-content/uploads/2018/03/beeple-crap-art-renders-03.jpg"
+          src={piece.photo_profile}
           alt=""
         />
-        <div>@deeple</div>
+        <div>{piece.username}</div>
+      </div>
       </div>
       <div className={styles.homeSingleArtworkPrice}>
         {piece.onSale ? "Price" : "Sold For"}
