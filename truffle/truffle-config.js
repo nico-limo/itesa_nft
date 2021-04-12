b84ee411801dba34d3c3f85be2032578aaf93ef4
toiaa <victoria@geddes.com.ar>
2021-04-12 16:11:48 -0300

    truffle starter config

---
+++ truffle/truffle-config.js
@@ -0,0 +1,40 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+if (!mnemonic || mnemonic.split(' ').length !== 12) {
+  throw new Error('unable to retrieve mnemonic from .secret');
+}
+
+const gasPriceTestnetRaw = fs.readFileSync(".gas-price-testnet.json").toString().trim();
+const gasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result, 16);
+if (typeof gasPriceTestnet !== 'number' || isNaN(gasPriceTestnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+}
+console.log("Gas price Testnet: " + gasPriceTestnet);
+
+const path = require("path");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 3000,
+      network_id: "*"
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.testnet.rsk.co/'),
+      network_id: 31,
+      gasPrice: Math.floor(gasPriceTestnet * 1.1),
+      networkCheckTimeout: 1e9
+    },
+  },
+
+  contracts_build_directory: path.join(__dirname, "/truffle/contracts"),
+
+  compilers: {
+    solc: {
+      version: "0.8.3",
+    }
+  }
+}
