30c4a3dc5108aefbee319925cfb53b26e1cf6f84
PabloMunaretto <pablo.munaretto@gmail.com>
2021-04-13 02:42:14 -0300

    avance test y contract

--- truffle/test/CryptoArtTest.js
+++ truffle/test/CryptoArtTest.js
@@ -4,37 +4,37 @@ contract("CRA", accounts => {
     var owner = accounts[0];
     var user = accounts[1];
     var contract;
-    beforeEach(async () => {
+    beforeEach(async function () {
         contract = await definition.new({from: owner})
     })
 
-    it("mint!", async () => {
+    it("mint!", async function () {
         let tx = await contract.mint("ipfs hash", web3.toWei(.001, "ether", {from: owner}))
         let tokenId = tx.logs[0].args.tokenId;
 
         assert.equal(await contract.ownerOf(tokenId), contract.address);
-    })
+    }),
 
-    it("buy token!", async () => {
+    it("buy token!", async function () {
         let tx = await contract.mint("ipfs hash", web3.toWei(.001, "ether", {from: owner}))
         let tokenId = tx.logs[0].args.tokenId;
 
         let tx2 = await contract.buyCard(tokenId, {from: user, value: web3.toWei(.001, "ether")})
         assert.equal(await contract.ownerOf(tokenId), user)
-    }
+    }),
 
-    it("gets ipfs hash", async () => {
+    it("gets ipfs hash", async function () {
         let tx = await contract.mint("ipfs hash", web3.toWei(.001, "ether", {from: owner}))
         let tokenId = tx.logs[0].args.tokenId;
 
         assert.equal(await contract.getIpfsHash(tokenId), "ipfs hash");
-    })
+    }),
 
-    it("gets tokens of", async () => {
+    it("gets tokens of", async function () {
         await contract.mint("ipfs hash!!", web3.toWei(.001, "ether", {from: owner}))
         await contract.mint("ipfs hash", web3.toWei(.001, "ether", {from: owner}))
 
-        assert.equal(await contract.tokensOf(contract.address), 2);
+        assert.equal(await contract.tokensOf(contract.address).length, 2);
     })
 
 })
